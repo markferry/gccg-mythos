0b9d77a5f35a1d36ffeaa97bdcf7a4a0bb10ce1b
mr-photon <mr-photon@634a58c5-d05b-43e7-87bc-c68b3b3f1831>
2011-02-05 14:17:41 +0000

    git-svn-id: https://gccg.svn.sourceforge.net/svnroot/gccg@90 634a58c5-d05b-43e7-87bc-c68b3b3f1831

--- core/Nr.bat
+++ core/Nr.bat
@@ -4,4 +4,4 @@ set USER=Windows
 set CLIENT=.\ccg_client.exe
 if exist .\home set HOME=.\home
 if exist module_windows32\ccg_client.exe set CLIENT=module_windows32\ccg_client.exe
-start %CLIENT% --user %USER% %1 %2 %3 %4 %5 %6 %7 %8 %9  Nr.xml
+start %CLIENT% --server lakka.kapsi.fi --port 61759 --full --geometry 1024x768 --user %USER% %1 %2 %3 %4 %5 %6 %7 %8 %9  Nr.xml

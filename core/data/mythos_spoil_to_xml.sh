708022ffbe4c5a53acd45f10094f4c72e7dd9841
Mark Ferry <mark@markferry.net>
2011-07-15 18:35:15 +0100

    Script and XSLT for exporting mythosdb CSV to GCCG XML

---
+++ core/data/mythos_spoil_to_xml.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+#
+# mythos_spoil_to_xml.sh SRC
+#    SRC: source CSV file
+#
+
+SRC="$1"
+
+# Extra processing to keep GCCG happy
+SEDSCRIPT="s/&apos;/'/g"
+
+# csv2xml needs a root element
+HEADER="<cards>"
+FOOTER="</cards>"
+XSLT="mythosdb_to_xml.xslt"
+XSLTPROC="xsltproc"
+
+# Map database SET IDs to GCCG set vars
+#
+get_setfname()
+{
+    case $1 in
+        "Limited")
+            SET_NAME="Limited"
+            SET_FNAME="ltd.xml"
+            SET_ABBREV="LTD"
+            ;;
+        "B1")
+            SET_NAME="Expeditions of Miskatonic University"
+            SET_FNAME="eomu.xml"
+            SET_ABBREV="EOMU"
+            ;;
+        "B2")
+            SET_NAME="Cthulhu Rising"
+            SET_FNAME="cr.xml"
+            SET_ABBREV="CR"
+            ;;
+        "B3")
+            SET_NAME="Legends of the Necronomicon"
+            SET_FNAME="lotn.xml"
+            SET_ABBREV="LOTN"
+            ;;
+        "Standard")
+            SET_NAME="Standard Game"
+            SET_FNAME="msgs.xml"
+            SET_ABBREV="MSGS"
+            ;;
+        "Dreamlands")
+            SET_NAME="Dreamlands"
+            SET_FNAME="drm.xml"
+            SET_ABBREV="DRM"
+            ;;
+        "New Æon")
+            SET_NAME="New Æon"
+            SET_FNAME="nae.xml"
+            SET_ABBREV="NAE"
+            ;;
+        "ScoobyDoo")
+            SET_NAME="Scooby Doo - Where Are You?"
+            SET_FNAME="sd.xml"
+            SET_ABBREV="SD"
+            ;;
+        "Promo")
+            SET_NAME="Promotional Cards"
+            SET_FNAME="pr.xml"
+            SET_ABBREV="PR"
+            ;;
+        *)
+            SET_NAME="None"
+            SET_FNAME="none.xml"
+            SET_ABBREV="NONE"
+            ;;
+        esac
+}
+
+gen_xml_test() {
+echo "$XSLTPROC" \
+        --stringparam  setid "$SET_ID"  \
+        --stringparam  setabbrev "$SET_ABBREV" \
+        --stringparam  setname "$SET_NAME" \
+        "$XSLT" -
+}
+
+gen_xml() {
+(
+    echo $HEADER
+    cat "$SRC" | csv2xml | sed "$SEDSCRIPT"
+    echo $FOOTER
+) | "$XSLTPROC" \
+        --stringparam  setid "$SET_ID"  \
+        --stringparam  setabbrev "$SET_ABBREV" \
+        --stringparam  setname "$SET_NAME" \
+        "$XSLT" -
+}
+
+for SET_ID in Limited B1 B2 B3 Standard Dreamlands "New Æon" ScoobyDoo Promo
+do
+    get_setfname "$SET_ID"
+    gen_xml > "$SET_FNAME"
+done
+
---
+++ core/data/mythosdb_to_xml.xslt
@@ -0,0 +1,55 @@
+<xsl:stylesheet version="1.0" 
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<!-- Set ID in database -->
+<xsl:param name="setid"></xsl:param>
+<!-- Set abbreviation -->
+<xsl:param name="setabbrev"></xsl:param>
+<!-- Full name for the set -->
+<xsl:param name="setname"></xsl:param>
+
+<xsl:output omit-xml-declaration="no"
+            encoding="UTF-8"
+            doctype="ccg-setinfo"
+            doctype-system="../gccg-set.dtd"
+            indent="yes"/>
+<!--                                              --> 
+  <xsl:strip-space elements="*"/>
+
+
+<!-- Convert csv2xml output to GCCG set
+        Title_en, Image and Text_en child elements become
+        attributes. The rest become parameterized children -->
+
+  <xsl:template match="/">
+    <ccg-setinfo name="{$setname}" abbrev="{$setabbrev}" game="Mythos">
+        <cards>
+        <!-- Only cards from the specified set -->
+        <xsl:for-each select="cards/row[Set=$setid]">
+            <card name="{Title_en}" graphics="{Image}" text="{Text_en}" >
+                <attr value="{Rarity}" key="rarity" />
+                <attr value="{Set}" key="set"/>
+                <attr value="{Rarity}" key="rarity"/>
+                <attr value="{Type}" key="type"/>
+                <attr value="{Sub_Type}" key="subtype"/>
+                <attr value="{Value}" key="value"/>
+                <attr value="{Gate}" key="gate"/>
+                <attr value="{Private_Text}" key="private"/>
+                <attr value="{Region}" key="region"/>
+                <attr value="{Subregion}" key="subregion"/>
+                <attr value="{Education}" key="education"/>
+                <attr value="{San}" key="san"/>
+                <attr value="{Max_San}" key="sanmax"/>
+                <attr value="{Hand_Size}" key="hand"/>
+                <attr value="{Minimum}" key="handmin"/>
+                <attr value="{Maximum}" key="handmax"/>
+                <attr value="{Dimension}" key="dimension"/>
+                <attr value="{Unique}" key="unique"/>
+            </card>
+            <xsl:text>&#10;</xsl:text>
+        </xsl:for-each>
+        </cards>
+    </ccg-setinfo>
+  </xsl:template>
+</xsl:stylesheet>
+
